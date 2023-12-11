c09527e56fc4a903be911ca393fb453c4e6be26b
batopa <batopa@gmail.com>
2023-12-11 21:49:24 +0100

    feat: new module desc

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,7 @@
 import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
 
 // Module options TypeScript interface definition blabla blabla
+// description of module here
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
