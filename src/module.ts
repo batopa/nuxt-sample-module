f93d8c3fe86209891638fd413a46ce1706be3066
Alberto Pagliarini <batopa@gmail.com>
2023-12-11 20:22:25 +0100

    Test for release (#3)
    * chore: up com
    
    * feat: bla

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,6 @@
 import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
 
-// Module options TypeScript interface definition blabla blabla
+// Module options TypeScript interface definition blabla blabla bla bla
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
@@ -8,12 +8,12 @@ export default defineNuxtModule<ModuleOptions>({
     name: 'nuxt-sample-module',
     configKey: 'sampleModule'
   },
-  // Default configuration options of the Nuxt module
+  // Default configuration options of the Nuxt module cia'
   defaults: {},
   setup (options, nuxt) {
     const resolver = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack` jakshdkasdh
     addPlugin(resolver.resolve('./runtime/plugin'))
   }
 })
