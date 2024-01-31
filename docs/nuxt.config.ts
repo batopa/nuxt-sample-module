90074053d11db92d854ac90df6a4586b4c5f7bea
batopa <batopa@gmail.com>
2024-01-31 09:17:10 +0100

    fix: app base url for github pages

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -3,9 +3,9 @@ export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
   devtools: { enabled: true },
 
-  modules: [
-    // Remove it if you don't use Plausible analytics
-    // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible'
-  ]
+  app: {
+    baseURL: '/nuxt-sample-module/',
+  },
+
+  modules: []
 })
